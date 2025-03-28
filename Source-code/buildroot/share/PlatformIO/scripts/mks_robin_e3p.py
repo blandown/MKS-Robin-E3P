638c3d73ad71e10d6c607a0330aa7117a0495dd2
root <root@marlinbuild.lynoi.local>
2025-03-28 01:19:22 +0000

    Fix for latest pio

--- Source-code/buildroot/share/PlatformIO/scripts/mks_robin_e3p.py
+++ Source-code/buildroot/share/PlatformIO/scripts/mks_robin_e3p.py
@@ -2,10 +2,12 @@ import os
 Import("env")
 
 # Relocate firmware from 0x08000000 to 0x08007000
-for define in env['CPPDEFINES']:
-    if define[0] == "VECT_TAB_ADDR":
-        env['CPPDEFINES'].remove(define)
+env['CPPDEFINES'] = [define for define in env['CPPDEFINES'] if define[0] != "VECT_TAB_ADDR"]
 env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08007000"))
+#for define in env['CPPDEFINES']:
+#    if define[0] == "VECT_TAB_ADDR":
+#        env['CPPDEFINES'].remove(define)
+#env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08007000"))
 
 custom_ld_script = os.path.abspath("buildroot/share/PlatformIO/ldscripts/mks_robin_e3p.ld")
 for i, flag in enumerate(env["LINKFLAGS"]):
